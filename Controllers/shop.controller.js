0e2a8e75d28a11a2628358840579a44ee5d41d93
ons ben fadhel <ons.benfadhel@esprit.tn>
2017-04-19 14:24:33 +0200

    Commentaires

--- Controllers/shop.controller.js
+++ Controllers/shop.controller.js
@@ -1,15 +1,35 @@
 function ShopControllerFN($scope,$http) {
     $scope.showmean=true;
     $scope.products=[];
+    $scope.commentaires=[];
     $http.get("http://localhost:3003/api/produit").then(function(reponse){
         $scope.products=reponse.data;
         console.log($scope.products[0]);
     })
+      $http.get("http://localhost:3003/api/commentaire").then(function(reponse){
+            $scope.commentaires=reponse.data;
+
+        })
 
     $scope.showcomments = function (mean) {
         if(mean !=undefined)
             console.log(mean);
     }
+    
+    $scope.addcomment = function () {
+        var data = {
+            _id: $scope.comment._id,
+            commentaire: $scope.comment.commentaire
+        };
+        $http.post('http://localhost:3003/api/commentaire', data).success(function (data, status) {
+            console.log(status);
+            init();
+            alert('Comment added');
+        });
+        $location.path('/');
+    };
+    
+    
 }
 
 angular
